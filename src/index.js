af28221e7084a272da6b050bff4454a92bd0c130
matthew040519 <63332916+matthew040519@users.noreply.github.com>
2023-07-22 12:18:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-const router = createBrowserRouter([{ path: "/", element: App }]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(  
-  // <React.StrictMode>    
-    <RouterProvider router={router} />
-  // </React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
